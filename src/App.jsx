84f34bb551837f6535b1c9d1c4beed17fe30daed
RicardoAG0312 <ricardo41216383@gmail.com>
2025-03-25 12:25:04 -0500

    Ricardo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+    return (
+        <>
+            <section className="container">
+                <h1> Your Name  </h1>
+            </section>
+        </>
+    )
+}
+
+export default App
